2ceec034bf4210551e93c57cc6a1f8e0f471420b
Kamila <ksalimzh@gmail.com>
2025-02-17 16:32:23 +0500

    Initial Commit

---
+++ pizza/pizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pizzaApp.swift
+//  pizza
+//
+//  Created by kamila on 17.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct pizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
