b745d0d70bbb4d56e0f8ee8917d0c761338fc363
Kamila <ksalimzh@gmail.com>
2025-02-20 23:14:09 +0500

    Alerts added

--- pizza/MenuItemView.swift
+++ pizza/MenuItemView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MenuItemView: View {
     @State private var addedItem: Bool = false
     @Binding var item: MenuItem
+    @State var presentAlert: Bool = false
     @ObservedObject var orders: OrderModel
     var body: some View {
         VStack {
@@ -20,7 +21,7 @@ struct MenuItemView: View {
                     .fontWeight(.semibold)
                     .foregroundStyle(.ultraThickMaterial)
                     .padding(.leading)
-    
+                
                 
                 if let image = UIImage(named: "\(item.id)_lg") {
                     Image(uiImage: image)
@@ -29,7 +30,7 @@ struct MenuItemView: View {
                         .padding([.top, .bottom], 5)
                         .font(.largeTitle)
                         .cornerRadius(15)
-                        //.shadow(color: .teal, radius: 5, x: 8, y: 8)
+                    //.shadow(color: .teal, radius: 5, x: 8, y: 8)
                     //.clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
                 }else{
                     Image("surfboard_lg")
@@ -49,8 +50,7 @@ struct MenuItemView: View {
                 }
             }
             Button {
-                addedItem = true
-                orders.addOrder(item, quantity: 1)
+                presentAlert = true
             } label: { // HStack
                 Spacer()
                 Text(item.price, format: .currency(code: "KZT"))
@@ -59,9 +59,21 @@ struct MenuItemView: View {
             }
             .disabled(item.id < 0)
             .padding()
-                .background(.red, in: Capsule())
-                .foregroundStyle(.white)
-                .padding(5)
+            .background(.red, in: Capsule())
+            .foregroundStyle(.white)
+            .padding(5)
+//            .alert("Buy a \(item.name)", isPresented: $presentAlert) {
+//                Button("No", role: .cancel) {}
+//                Button("Yes") {
+//                    addedItem = true
+//                    orders.addOrder(item, quantity: 1)
+//                }
+//                Button("Make it a double") {
+//                    addedItem = true
+//                    orders.addOrder(item, quantity: 2)
+//                }
+//            }
+            
         }
     }
 }
